49592b190fdce03a389964e000f6128c2115a6cd
Svengali-tech <adambalogun301@gmail.com>
2026-03-01 12:46:24 -0500

    Initial Commit

---
+++ StockPortfolioAnalyzer/StockPortfolioAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPortfolioAnalyzerApp.swift
+//  StockPortfolioAnalyzer
+//
+//  Created by Adam Balogun on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct StockPortfolioAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
